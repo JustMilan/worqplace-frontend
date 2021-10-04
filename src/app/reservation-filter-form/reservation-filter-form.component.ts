0f16970eb9d46c5bf69b2a756fda5902cc3636ad
JustMilan <milandol321@gmail.com>
2021-10-05 01:00:57 +0200

    Add reservation filter form (WIP)

---
+++ src/app/reservation-filter-form/reservation-filter-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-filter-form',
+  templateUrl: './reservation-filter-form.component.html',
+  styleUrls: ['./reservation-filter-form.component.css']
+})
+export class ReservationFilterFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/reservation-page/reservation-page.component.ts
+++ src/app/reservation-page/reservation-page.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {LocationEnum} from "../locationEnum";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-reservation-page',
   templateUrl: './reservation-page.component.html',
-  styleUrls: ['./reservation-page.component.css']
+  styleUrls: ['./reservation-page.component.css'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class ReservationPageComponent implements OnInit {
+  locations: LocationEnum[] = [LocationEnum.Groningen, LocationEnum["Den Haag"]]
+  minDate: Date = new Date()
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onChange() {
+    //TODO: implement onchange listener. Every time a form parameter changes search results must be updated
+  }
 }
