c26bf0c3dcafc3e75e55a47dd13873088b71c064
JustMilan <milandol321@gmail.com>
2021-10-27 20:35:39 +0200

    Add data to table from backend

--- src/app/my-reservations/my-reservations.component.ts
+++ src/app/my-reservations/my-reservations.component.ts
@@ -9,20 +9,18 @@ import {Reservation} from "../interface/reservation";
 })
 export class MyReservationsComponent implements OnInit {
   allMyReservations: Reservation[];
-  columnsToDisplay = ['id', 'date', 'start time', 'end time', 'workplace', 'room'];
+  columnsToDisplay = ['id', 'date', 'startTime', 'endTime', 'workplaceId', 'roomId'];
 
   constructor(private reservationService: ReservationService) {
   }
 
-  getAllReservationsByEmployeeId(employeeId: number){
+  getAllReservationsByEmployeeId(employeeId: number) {
     employeeId = 1;
 
     this.reservationService.getAllReservationsByEmployeeId(employeeId).subscribe(reservations => this.allMyReservations = reservations);
   }
 
   ngOnInit(): void {
+    this.getAllReservationsByEmployeeId(1);
   }
-
-  //TODO: function that will get the 3 most recent reservations and if there
-  // is a fourth enables the empty card with the text and link "See other reservations"
 }
